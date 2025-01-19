0d386848c1528de6b35fa56fba7f60b0793b2f0d
zhimingp <zmingp@126.com>
2025-01-19 20:42:31 +0800

    Java单例模式

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        A a1 = A.getObject();
-        A a2 = A.getObject();
-        System.out.println(a1);
-        System.out.println(a2);
+
+
     }
 }
